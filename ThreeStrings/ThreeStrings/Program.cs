97eaa23f58beae91e04165d85b5cf72969bbe678
Jennifer Faron <jf891w@gmail.com>
2018-07-15 20:22:09 -0500

    Adding three strings

---
+++ ThreeStrings/ThreeStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ThreeStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Please answer the below questions.\n");
+			Console.Write("Enter your street number: ");
+			string streetNumber = Console.ReadLine();
+
+			Console.Write("Enter your street address: ");
+			string streetAddress = Console.ReadLine();
+
+			Console.Write("Enter your city: ");
+			string city = Console.ReadLine();
+
+			string address = streetNumber + " " + streetAddress + " " + city;
+
+			Console.WriteLine("\nYour address is " + address + ".");
+			Console.ReadLine();
+        }
+    }
+}
